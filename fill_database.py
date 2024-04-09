09e9e1f18aadf9b6712b44c9e2cb937111718e0e
d.rudenko <dimitriyrudenk@gmail.com>
2024-04-09 21:16:34 +0300

    Hydra-zen draft

--- fill_database.py
+++ fill_database.py
@@ -7,106 +7,108 @@ from qdrant_client import QdrantClient, models
 from qdrant_client.http.exceptions import UnexpectedResponse
 from tqdm import trange
 from transformers import CLIPModel, CLIPProcessor
+from hydra_zen import zen, builds, ZenStore
 
 warnings.filterwarnings(
     "ignore", category=UserWarning, message="TypedStorage is deprecated"
 )
 # Constants
-IMAGE_BASE_FOLDERS = ["./0", "./1/"]
-DEFAULT_BATCH_SIZE = 32
-DEFAULT_N_SAMPLES = 10000
-collection_name = os.getenv("COLLECTION_NAME") #"AdsDataset"
-qdrant_port = os.getenv("QDRANT_PORT") #6333
-qdrant_host = os.getenv("QDRANT_HOST") #qdrant
-model_name = os.getenv("MODEL_NAME") #"openai/clip-vit-base-patch32"
+IMAGE_BASE_FOLDERS = ["/app/data/0/", "/app/data/1/"]
 
-# Initialize CLIP model and processor
-model = CLIPModel.from_pretrained(model_name).eval()
-model = torch.compile(model)
-processor = CLIPProcessor.from_pretrained(model_name)
+class Processor:
+    def __init__(self, model_name: str,
+                 qdrant_host: str,
+                 qdrant_port: int,
+                 collection_name: str = None,
+                 compile: bool=True,
+                 batch_size:int=32):
+        self.model_preprocessor = CLIPProcessor.from_pretrained(model_name)
+        self.model = CLIPModel.from_pretrained(model_name)
+        if compile:
+            self.model = torch.compile(self.model)
 
+        self.db_client = QdrantClient(url=f"http://{qdrant_host}:{qdrant_port}")
+        self.collection_name = collection_name
+        self.batch_size=batch_size
 
-# Function to get image embeddings
-def get_image_embedding(path: str):
-    image = Image.open(path)
-    inputs = processor(images=image, return_tensors="pt")
-    outputs = model.get_image_features(**inputs)
-    return outputs
-
-
-# Function to load batch to Qdrant
-def load_batch_to_qdrant(client: QdrantClient,
-                         paths: list[str],
-                         batch: list[list[float]],
-                         collection : str = collection_name,
-                         ids: list[int] = None):
-    client.upsert(
-        collection_name=collection,
-        points=models.Batch(
-            payloads=[{"path": path} for path in paths],
-            vectors=batch,
-            ids=ids
-        ),
-    )
+        self.create_collection()
 
 
-# Main function
-def main(BATCH_SIZE: int, n_samples: int):
-    # Initialize Qdrant client
-    client = QdrantClient(url=f"http://{qdrant_host}:{qdrant_port}")
+    def get_image_embedding(self, path: str):
+        image = Image.open(path)
+        inputs = self.model_preprocessor(images=image, return_tensors="pt")
+        outputs = self.model.get_image_features(**inputs)
+        return outputs
 
-    # Create collection if not exists
-    try:
-        client.create_collection(
-            collection_name=collection_name,
-            vectors_config=models.VectorParams(
-                size=512, distance=models.Distance.COSINE
-            ),
-            optimizers_config=models.OptimizersConfigDiff(
-                indexing_threshold=0,
+    def load_batch_to_qdrant(self, paths: list[str],
+                             batch: list[list[float]],
+                             ids: list[int] = None):
+        self.db_client.upsert(
+            collection_name=self.collection_name,
+            points=models.Batch(
+                payloads=[{"path": path} for path in paths],
+                vectors=batch,
+                ids=ids
             ),
         )
-    except UnexpectedResponse as e:
-        print(e)
+    def process(self, file_paths: list[str]):
+        for batch_start in trange(0, len(file_paths), self.batch_size):
+            batch_end = min(batch_start + self.batch_size, len(file_paths))
+            batch_paths = file_paths[batch_start:batch_end]
+            batch_images = [Image.open(path) for path in batch_paths]
+            batch_ids = list(range(batch_start, batch_end))
 
-    # Load images and their paths
+            inputs = self.model_preprocessor(images=batch_images, return_tensors="pt")
+            outputs = self.model.get_image_features(**inputs).detach().numpy()
+
+            self.load_batch_to_qdrant(
+                paths=batch_paths,
+                batch=outputs.tolist(),
+                ids=batch_ids
+            )
+
+    def create_collection(self):
+        try:
+            self.db_client.create_collection(
+                collection_name=self.collection_name,
+                vectors_config=models.VectorParams(
+                    size=512, distance=models.Distance.COSINE
+                ),
+                optimizers_config=models.OptimizersConfigDiff(
+                    indexing_threshold=0,
+                ),
+            )
+        except UnexpectedResponse as e:
+            print(e)
+
+# Main function
+def main(BATCH_SIZE: int = 32,
+         model_name: str = "openai/clip-vit-base-patch32",
+         qdrant_host: str = "qdrant",
+         qdrant_port: int = 6333,
+         collection_name: str = "DEFAULT_COLLECTION"
+         ):
     file_paths = [
         os.path.join(folder, image)
         for folder in IMAGE_BASE_FOLDERS
         for image in os.listdir(folder)
-    ][:n_samples]
-
-    # Process images in batches
-    for batch_start in trange(0, len(file_paths), BATCH_SIZE):
-        batch_end = min(batch_start + BATCH_SIZE, len(file_paths))
-        batch_paths = file_paths[batch_start:batch_end]
-        batch_images = [Image.open(path) for path in batch_paths]
-        batch_ids = list(range(batch_start, batch_end))
-
-        inputs = processor(images=batch_images, return_tensors="pt")
-        outputs = model.get_image_features(**inputs).detach().numpy()
-
-        load_batch_to_qdrant(
-            client=client, paths=batch_paths, batch=outputs.tolist(), ids=batch_ids
-        )
+    ]
+    processor = Processor(model_name=model_name,
+                          qdrant_host=qdrant_host,
+                          qdrant_port=qdrant_port,
+                          batch_size=BATCH_SIZE,
+                          collection_name=collection_name
+                          )
+    processor.process(file_paths)
 
 
 # Entry point
+MainConfig = builds(main, populate_full_signature=True)
+store = ZenStore()
+store(MainConfig, name="load")
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process images and load to Qdrant.")
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=DEFAULT_BATCH_SIZE,
-        help="Batch size for processing images",
-    )
-    parser.add_argument(
-        "--n_samples",
-        type=int,
-        default=DEFAULT_N_SAMPLES,
-        help="Number of samples to process",
-    )
-
-    args = parser.parse_args()
-
-    main(args.batch_size, args.n_samples)
+    store.add_to_hydra_store()
+    zen(main).hydra_main(config_name="load",
+                         version_base="1.1",
+                         config_path="./config",
+                         )
--- main_page.py
+++ main_page.py
@@ -1,10 +1,10 @@
 import logging
 import warnings
-import os
 import gradio as gr
 from qdrant_client import QdrantClient
-import torch
 from transformers import CLIPModel, CLIPProcessor
+from hydra_zen import zen, builds, ZenStore
+from itertools import islice
 
 warnings.filterwarnings(
     "ignore", category=UserWarning, message="TypedStorage is deprecated"
@@ -12,72 +12,69 @@ warnings.filterwarnings(
 
 logger = logging.getLogger(__name__)
 
-def_model = os.getenv("MODEL_NAME")
-qdrant_port = os.getenv("QDRANT_PORT")
-qdrant_host = os.getenv("QDRANT_HOST")
-collection_name = os.getenv("COLLECTION_NAME")
+class Processor:
+    def __init__(self, model_name: str, qdrant_host: str, qdrant_port: int, res_len: int = 6, collection_name: str = None):
+        self.processor = CLIPProcessor.from_pretrained(model_name)
+        self.model = CLIPModel.from_pretrained(model_name)
+        self.db_client = QdrantClient(url=f"http://{qdrant_host}:{qdrant_port}")
+        self.res_len = res_len
+        self.collection_name = collection_name
 
-frontend_name = os.getenv("FRONTEND_HOST") #"0.0.0.0"
-frontend_port = os.getenv("FRONTEND_PORT") #7860
-
-processor = None
-model = None
-client = None
-
-
-def search_by_query(input_str: str):
-    processed_input = processor(text=input_str, return_tensors="pt")
-    query = model.get_text_features(**processed_input)
-    query = query.detach().tolist()[0]
-    res = client.search(
-        collection_name=collection_name,
-        query_vector=query,
-        with_vectors=False,
-        with_payload=True,
-    )
-
-    return [elem.payload["path"] for elem in res[:6]]
+    def process(self, query: str):
+        processed_input = self.processor(text=query, return_tensors="pt")
+        query = self.model.get_text_features(**processed_input)
+        query = query.detach().tolist()[0]
+        res = self.db_client.search(
+            collection_name=self.collection_name,
+            query_vector=query,
+            with_payload=True,
+        )
+        return [elem.payload["path"] for elem in islice(res, self.res_len)]
 
 
-with gr.Blocks() as demo:
-    textbox = gr.Textbox(
-        label="Search through the Advertisement Image Dataset",
-        info="Text promt",
-        lines=1,
-        value="Pizza",
-        render=True,
-        interactive=True,
-    )
-    btn = gr.Button("Search", scale=0)
-    gallery = gr.Gallery(
-        label="Generated images",
-        show_label=False,
-        elem_id="gallery",
-        columns=[3],
-        rows=[1],
-        object_fit="contain",
-        height="auto",
-        # render=False,
-        interactive=False,
-    )
-    btn.click(search_by_query, inputs=textbox, outputs=gallery)
+def main(server_name: str = "0.0.0.0",
+         server_port: int = 7860,
+         model_name: str = "openai/clip-vit-base-patch32",
+         qdrant_host: str = "qdrant",
+         qdrant_port: int = 6333,
+         collection_name: str = "DEFAULT_COLLECTION"):
+    processor = Processor(model_name, qdrant_host, qdrant_port, collection_name=collection_name)
+    with gr.Blocks() as demo:
+        textbox = gr.Textbox(
+            label="Search through the Advertisement Image Dataset",
+            info="Text promt",
+            lines=1,
+            value="Pizza",
+            render=True,
+            interactive=True,
+        )
+        btn = gr.Button("Search", scale=0)
+        gallery = gr.Gallery(
+            label="Generated images",
+            show_label=False,
+            elem_id="gallery",
+            columns=[3],
+            rows=[1],
+            object_fit="contain",
+            height="auto",
+            # render=False,
+            interactive=False,
+        )
+        btn.click(processor.process, inputs=textbox, outputs=gallery)
 
-
-def boot(model_name: str = def_model):
-    global processor, model, client
-
-    processor = CLIPProcessor.from_pretrained(model_name)
-    model = CLIPModel.from_pretrained(model_name)
-    model = torch.compile(model)
-    client = QdrantClient(url=f"http://{qdrant_host}:{qdrant_port}")
-
-
-if __name__ == "__main__":
-    boot()
-    print(frontend_name)
-    print(frontend_port)
     demo.launch(
-        server_name=frontend_name,
-        server_port=frontend_port,
+        server_name=server_name,
+        server_port=server_port,
         share=True,
     )
+
+
+MainConfig = builds(main, populate_full_signature=True)
+store = ZenStore()
+store(MainConfig, name="search")
+if __name__ == "__main__":
+    store.add_to_hydra_store()
+    zen(main).hydra_main(config_name="search",
+                         version_base="1.1",
+                         config_path="./config",
+                         )
